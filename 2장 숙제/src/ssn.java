3f7774c36ad1ba30050d72f5d44f7ef5e5b73677
dldkffh <79070302+dldkffh@users.noreply.github.com>
2021-02-15 22:17:38 +0900

    Delete 2장 숙제 directory

--- 2장 숙제/src/ssn.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class ssn {
-	public static void main(String[]args){
-		System.out.println("주민버호를 입력하시오.");
-		Scanner a = new Scanner(System.in).useDelimiter("\\s|-");
-		int ssnum = a.nextInt();
-		System.out.println(ssnum);
-	}
-}
